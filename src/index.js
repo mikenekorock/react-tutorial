c5a6540138df38e8b80da7402f124e1a9cd7eb05
mikenekorock <golden.lucky777@gmail.com>
2019-12-26 19:38:57 +0900

    stateって各コンポーネント内でプライベートとみなすべきもの

--- src/index.js
+++ src/index.js
@@ -23,6 +23,13 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   renderSquare(i) {
     return <Square value={i} />;
   }
